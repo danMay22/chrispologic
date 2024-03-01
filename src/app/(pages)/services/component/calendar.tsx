aab283edc17e4c68d7f4d74ec34ce7f38bd4cb1c
Dan Mayunga <44951241+danMay22@users.noreply.github.com>
2024-03-01 07:30:20 +0200

    29.03

--- src/app/(pages)/services/component/calendar.tsx
+++ src/app/(pages)/services/component/calendar.tsx
@@ -1,13 +1,52 @@
 import Image from 'next/image';
 const stats = [
-    { id: 1, name: 'Weekly and Prayer and Fasting Meeting @6:30pm', value: '06 February 2024' },
-    { id: 2, name: 'Weekly and Prayer and Fasting Meeting @6:30pm', value: '13 February 2024' },
-    { id: 3, name: 'Valentines Theme Services/New Memebership Class', value: '18 February 2024' },
-    { id: 4, name: 'Weekly and Prayer and Fasting Meeting @6:30pm', value: '20 February 2024' },
-    { id: 2, name: 'Baby Dedication/Board Meeting', value: '25 February 2024' },
-    { id: 3, name: 'Weekly and Prayer and Fasting Meeting @6:30pm', value: '27 February 2024' },
-    { id: 4, name: 'Monthly Membership Service', value: '29 February 2024' }
-  ]
+  {
+    id: 1,
+    name: 'Weekly and Prayer and Fasting Meeting @6:30pm',
+    value: '02 March 2024',
+  },
+  {
+    id: 2,
+    name: 'Weekly and Prayer and Fasting Meeting @6:30pm',
+    value: '09 March 2024',
+  },
+  {
+    id: 3,
+    name: 'AGM Members Meeting',
+    value: '14 March 2024',
+  },
+  {
+    id: 4,
+    name: 'Weekly and Prayer and Fasting Meeting @6:30pm',
+    value: '16 March 2024',
+  },
+  { id: 5, name: 'Couples Seminar', value: '20 March 2024' },
+  {
+    id: 6,
+    name: 'New Membership Class',
+    value: '21 March 2024',
+  },
+  {
+    id: 7,
+    name: 'Weekly and Prayer and Fasting Meeting @6:30pm',
+    value: '23 March 2024',
+  },
+  {
+    id: 8,
+    name: 'Monthly Membership Service',
+    value: '25 March 2024',
+  },
+  {
+    id: 9,
+    name: 'Board Meeting',
+    value: '28 March 2024',
+  },
+  {
+    id: 10,
+    name: 'Weekly and Prayer and Fasting Meeting @6:30pm',
+    value: '30 March 2024',
+  },
+];
 
 export default function Calendar() {
   return (
@@ -17,39 +56,35 @@ export default function Calendar() {
           <div className='mx-auto max-w-2xl'>
             <div className='max-w-lg'>
               <h1 className='mt-10 text-4xl font-bold tracking-tight text-foreground sm:text-6xl'>
-               February events
+                March events
               </h1>
-              <dl className="mt-16 grid max-w-xl grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 xl:mt-16">
-              {stats.map((stat) => (
-                <div key={stat.id} className="flex flex-col gap-y-3 border-l border-gray-900/10 pl-6">
-                  <dt className="text-sm leading-6 text-gray-600">{stat.name}</dt>
-                  <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900">{stat.value}</dd>
-                </div>
-              ))}
-            </dl>
+              <dl className='mt-16 grid max-w-xl grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 xl:mt-16'>
+                {stats.map((stat) => (
+                  <div
+                    key={stat.id}
+                    className='flex flex-col gap-y-3 border-l border-gray-900/10 pl-6'
+                  >
+                    <dt className='text-sm leading-6 text-gray-600'>
+                      {stat.name}
+                    </dt>
+                    <dd className='order-first text-3xl font-semibold tracking-tight text-gray-900'>
+                      {stat.value}
+                    </dd>
+                  </div>
+                ))}
+              </dl>
             </div>
           </div>
         </div>
         <div className='mt-20 sm:mt-24 md:mx-auto md:max-w-2xl lg:mx-0 lg:mt-0 lg:w-screen'>
-          <div className='bg-background border h-[48rem]  relative rounded-lg shadow-sm'>
+          <div className='bg-background border h-[48rem] relative rounded-lg shadow-sm'>
             <Image
               className='h-full w-full object-contain rounded-sm'
-              src='/Calendar/Screenshot(50).png'
+              src='/Calendar/Screenshot(51).png'
               alt='hero section image'
               layout='fill'
             />
           </div>
-        {/*   <div className="mt-10 absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-        <a
-          href='#'
-          className="btn btn-circle h-20 w-20"
-        >
-          ❮ 
-        </a>
-        <a href='#' className="btn btn-circle">
-         next  ❯ 
-        </a>
-      </div> */}
         </div>
       </div>
     </section>
