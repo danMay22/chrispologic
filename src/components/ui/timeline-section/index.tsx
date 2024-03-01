21c3cd56b49307bbee20033c4d5314a66815ad67
Dan Mayunga <44951241+danMay22@users.noreply.github.com>
2024-03-01 07:43:18 +0200

    01.03

--- src/components/ui/timeline-section/index.tsx
+++ src/components/ui/timeline-section/index.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function TimelineSection() {
   return (
-    <section className='bg-[#0229a8] sm:h-[32rem] h-48 relative'>
+    <section className='bg-[#001457] sm:h-[32rem] h-45 relative'>
       <Image
         className='h-full w-full sm:object-contain object-cover rounded-sm'
         src='/images/home/timeline-sectioon.jpg'
