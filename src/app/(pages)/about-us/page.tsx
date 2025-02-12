413547516d970ebe2879acfdb459d6c266b2939d
danMay22 <mayunga.dan22@gmail.com>
2025-02-12 17:02:42 +0200

    about page

--- src/app/(pages)/about-us/page.tsx
+++ src/app/(pages)/about-us/page.tsx
@@ -1,7 +1,186 @@
 import Team from '@/components/ui/team';
+import { Blocks, HeartHandshake, MicVocal, UserRound } from 'lucide-react';
+import Image from 'next/image';
+import BannerSection from './components/banner-section';
+
+export default function AboutPage() {
+  return (
+    <section className='py-32'>
+      <div className='container flex flex-col gap-28'>
+        <div className='flex flex-col gap-7'>
+          <BannerSection />
+          <h1 className='text-4xl font-semibold lg:text-7xl'>
+            Empowers, support and help share Christ's love through worship,
+            outreach, and service.
+          </h1>
+          <p className='max-w-xl text-lg'>
+            Building strong relationships - creating an environment where people
+            can form meaningful connections, support each other, and grow
+            together as a spiritual family.
+          </p>
+        </div>
+        <div className='grid gap-6 md:grid-cols-2'>
+          <Image
+            src={'/christ.jpeg'}
+            alt='clock'
+            className='w-full aspect-square rounded-lg object-cover bg-secondary'
+            width={600}
+            height={600}
+          />
+          <div className='flex flex-col justify-between gap-10 rounded-2xl bg-muted p-10'>
+            <p className='text-2xl text-muted-foreground'>OUR MISSION</p>
+            <p className='text-lg font-medium'>
+              At Yonder, we are more than just a church - we are a vibrant
+              family united in our love for God. Nestled in the heart of
+              Midrand, South Africa, we celebrate our diversity through our
+              multicultural congregation that brings together people from all
+              walks of life. We believe that spirituality and joy go hand in
+              hand. Our services are a powerful fusion of dynamic preaching that
+              touches hearts and transforms lives, complemented by an
+              exceptional music ministry that raises the standard of worship.
+              Excellence is not just a goal at Yonder - it's who we are. In
+              everything we do, from our weekly services to our community
+              outreach, we strive to honor God through our commitment to
+              excellence. As a rapidly growing congregation, we are driven by a
+              bold and ambitious vision. We believe that God has called us to
+              make a lasting impact not just in Midrand, but throughout South
+              Africa and beyond. Our growth is not just in numbers, but in the
+              depth of our faith, the strength of our community, and the reach
+              of our influence. Together, we are building a spiritual home where
+              everyone is welcome, where faith is lived out with authenticity,
+              and where lives are transformed through the power of God's love.
+            </p>
+          </div>
+        </div>
+        <Team />
+        <div className='flex flex-col gap-6 md:gap-20'>
+          <div className='max-w-xl'>
+            <h2 className='mb-2.5 text-3xl font-semibold md:text-5xl'>
+              We at Yonder are committed to:
+            </h2>
+            <p className='text-muted-foreground'>
+              A focus on spiritual growth and discipleship throughout all of our
+              ministries. With main goal of helping members deepen their faith
+              and relationship with God through services, worship, prayer, and
+              Bible study.
+            </p>
+          </div>
+          <div className='grid gap-10 md:grid-cols-4'>
+            <div className='flex flex-col'>
+              <div className='mb-5 flex size-12 items-center justify-center rounded-2xl bg-accent'>
+                <MicVocal className='size-8' />
+              </div>
+              <h3 className='mb-3 mt-2 text-lg font-semibold'>
+                Music Ministry: <br />
+                Worship Team
+              </h3>
+              <p className='font-medium text-muted-foreground'>
+                {' '}
+                Our Worship and Music Ministry create an atmosphere of authentic
+                worship that transcends cultural boundaries and brings people
+                into God's presence. With skilled musicians and vocalists who
+                understand both the technical and spiritual aspects of leading
+                worship. The team aim to incorporate diverse musical styles that
+                reflect the congregation's multicultural makeup while
+                maintaining excellence in execution. Regular rehearsals,
+                spiritual devotions, and technical training sessions are
+                essential to maintain high standards.
+                <br /> contact us if you'd like to join the team or for more
+                information.
+              </p>
+            </div>
+            <div className='flex flex-col'>
+              <div className='mb-5 flex size-12 items-center justify-center rounded-2xl bg-accent'>
+                <UserRound className='size-8' />
+              </div>
+              <h3 className='mb-3 mt-2 text-lg font-semibold'>
+                The Welcome Ministry: <br />
+                Hospitality squad
+              </h3>
+              <p className='font-medium text-muted-foreground'>
+                {' '}
+                This team serves as the church's first point of contact,
+                creating an environment where every visitor feels valued and
+                welcomed from the moment they arrive. This ministry develop a
+                systematic approach to greeting, from the parking lot to the
+                sanctuary, with trained volunteers who can assist newcomers,
+                answer questions, and help people find their way. The team also
+                establish a follow-up system for first-time visitors, including
+                welcoming quick session, connection cards, and personal
+                follow-up calls or visits (if need be).
+              </p>
+            </div>
+            <div className='flex flex-col'>
+              <div className='mb-5 flex size-12 items-center justify-center rounded-2xl bg-accent'>
+                <HeartHandshake className='size-8' />
+              </div>
+              <h3 className='mb-3 mt-2 text-lg font-semibold'>
+                Community Outreach Programs:
+                <br />
+                Moving the needle
+              </h3>
+              <p className='font-medium text-muted-foreground'>
+                Focus on meeting both spiritual and practical needs in Midrand
+                and surrounding areas. The ministry will identify specific
+                community needs and develop targeted programs such as food
+                distribution, educational support, skills development workshops,
+                and counseling services. Building partnerships with local
+                organizations and maintaining consistent engagement with the
+                community helps establish the church as a reliable source of
+                support and hope.
+              </p>
+            </div>
+            <div className='flex flex-col'>
+              <div className='mb-5 flex size-12 items-center justify-center rounded-2xl bg-accent'>
+                <Blocks className='size-8' />
+              </div>
+              <h3 className='mb-3 mt-2 text-lg font-semibold'>
+                The Youth and Children's Ministry: <br />
+                The team for the future
+              </h3>
+              <p className='font-medium text-muted-foreground'>
+                create age-appropriate programs that make faith relevant and
+                exciting for young people. This includes developing curriculum
+                that balances biblical teaching with practical life application,
+                organizing engaging activities that foster spiritual growth, and
+                training dedicated teachers who can mentor young people
+                effectively.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className='grid gap-10 md:grid-cols-2'>
+          <div>
+            <p className='mb-10 text-sm font-medium text-muted-foreground'>
+              JOIN ANY OUR TEAM
+            </p>
+            <h2 className='mb-2.5 text-3xl font-semibold md:text-5xl'>
+              And together, we&apos;re going to change lifes and make real
+              impact.
+            </h2>
+          </div>
+          <div>
+            <Image
+              src={'/images/gallery/Church Events/IMG-20231027-WA0004.jpg'}
+              alt='clock'
+              className='mb-6 max-h-36 w-full rounded-xl object-cover'
+              width={600}
+              height={600}
+            />
+            <p className='text-muted-foreground'>
+              And we&apos;re looking for the right people to help us do it. If
+              you&apos;re passionate about chirst&apos;s love and the desire to
+              help make change in the world, this might be the place for you
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+  /* 
+import Team from '@/components/ui/team';
 import { Metadata } from 'next';
 import AboutUsDescriptionSection from './components/about-description-section';
-import BannerSection from './components/banner-section';
 import ChurchServices from './components/church';
 import { default as Note } from './components/mission';
 
@@ -34,4 +213,6 @@ export default function Page() {
       </div>
     </>
   );
+}
+*/
 }
--- src/components/ui/team/index.tsx
+++ src/components/ui/team/index.tsx
@@ -1,52 +1,65 @@
+import Image from 'next/image';
+
 const people = [
   {
     name: 'Bakhe Dlamini',
-    role: 'Pastor',
     imageUrl: '/therapy_photo.jpg',
-    bio: 'Bakhe is the senior founding pastor of Yonder Worship Centre. He is a world-class speaker, leader, Community Psychologist, and Author. He is a much-sought-after speaker and marriage therapist. He is well known throughout South Africa and beyond its borders with regards to his work on families and love relationships. Bakhe is part of Married at First Sight and XO (couples) Goals, which are two TV shows that broadcast on Mzansi Magic and DSTV. Bakhe runs a successful private practice in Sandton. He holds a degree in theology from Global University, Missouri, USA, and a Masters degree in community psychology from MAGU, Malawi.',
+    title: 'Senior Founding Pastor of Yonder Worship Centre.',
+    bio: '',
   },
   {
     name: 'Nokwanda Dlamini',
-    role: 'Pastor',
     imageUrl: '/IMG.30.42_9ad4bbc8.jpg',
-    bio: 'Nokwanda is a world-class leader and speaker. Alongside her husband, Bakhe, she runs their private practice in Sandton. She is a Community Psychologist, marriage and family Therapist. She holds a degree in social works from the University of KwaZulu-Natal and a Masters degree in Community Psychology from MAGU, Malawi. Nokwanda is part of the panel of experts cast for Married at First Sight SA, a popular TV show that broadcasts on Mzansi Magic and DSTV.',
+    title: 'Pastor of Yonder Worship Centre.',
+    bio: '',
   },
 ];
 export default function Team() {
   return (
-    <section className='py-20 isolate overflow-hidden bg-white px-4 lg:px-40'>
-      <div className='w-full max-w-xl lg:shrink-0 xl:max-w-2xl'>
-        <div className='max-w-2xl xl:col-span-2'>
-          <h2 className='py-5 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
-            Our Pastors
-          </h2>
-        </div>
-        <ul
-          role='list'
-          className='-mt-12 space-y-12 divide-y divide-gray-200 xl:col-span-3'
-        >
-          {people.map((person) => (
-            <li
-              key={person.name}
-              className='flex flex-col gap-10 pt-12 sm:flex-row'
-            >
-              <img
-                className='aspect-[4/5] w-52 flex-none rounded-2xl object-cover'
-                src={person.imageUrl}
-                alt=''
-              />
-              <div className='max-w-xl flex-auto'>
-                <h3 className='text-lg font-semibold leading-8 tracking-tight text-gray-900'>
-                  {person.name}
-                </h3>
-                <p className='mt-6 text-base leading-7 text-gray-600'>
-                  {person.bio}
-                </p>
-              </div>
-            </li>
-          ))}
-        </ul>
+    <div className='flex flex-col items-center justify-center py-14 px-4 sm:px-6 lg:px-8'>
+      <h2 className='mt-3 text-4xl sm:text-5xl font-bold tracking-tight'>
+        Our Leardership
+      </h2>
+      <div className='mt-20 w-full grid grid-cols-1 sm:grid-cols-2 gap-12 max-w-screen-lg mx-auto'>
+        <p className='mt-4 text-base sm:text-lg'>
+          <strong>Bakhe: </strong> He is a world-class speaker, leader,
+          Community Psychologist, and Author. He is a much-sought-after speaker
+          and marriage therapist. <br />
+          He is well known throughout South Africa and beyond its borders with
+          regards to his work on families and love relationships. Bakhe is part
+          of Married at First Sight and XO (couples) Goals, which are two TV
+          shows that broadcast on Mzansi Magic and DSTV. Bakhe runs a successful
+          private practice in Sandton. He holds a degree in theology from Global
+          University, Missouri, USA, and a Masters degree in community
+          psychology from MAGU, Malawi.
+        </p>
+
+        <p className='mt-4 text-base sm:text-lg'>
+          <strong>Nokwanda: </strong> is a world-class leader and speaker.
+          Alongside her husband, Bakhe, she runs their private practice in
+          Sandton. She is a Community Psychologist, marriage and family
+          Therapist. She holds a degree in social works from the University of
+          KwaZulu-Natal and a Masters degree in Community Psychology from MAGU,
+          Malawi. Nokwanda is part of the panel of experts cast for Married at
+          First Sight SA, a popular TV show that broadcasts on Mzansi Magic and
+          DSTV.
+        </p>
       </div>
-    </section>
+      <div className='mt-20 w-full grid grid-cols-1 sm:grid-cols-2 gap-12 max-w-screen-lg mx-auto'>
+        {people.map((member) => (
+          <div key={member.name} className='text-center'>
+            <Image
+              src={member.imageUrl}
+              alt={member.name}
+              className='h-20 w-20 rounded-full object-cover mx-auto bg-secondary'
+              width={120}
+              height={120}
+            />
+            <h3 className='mt-4 text-lg font-bold'>{member.name}</h3>
+            <p className=''>{member.title}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
